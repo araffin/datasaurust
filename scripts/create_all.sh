fc4375b8b53e12b844aa386dbd35c516c239ce14
Antonin Raffin <antonin.raffin@ensta.org>
2023-04-02 21:40:32 +0200

    Finetune params

--- scripts/create_all.sh
+++ scripts/create_all.sh
@@ -3,27 +3,28 @@
 # Output folder for videos
 video_folder=logs/videos_0
 mkdir -p $video_folder
-log_folders=(cat dog rabbit whale)
+log_folders=(cat dog whale rabbit)
 
 # Remove old logs
 for log_folder in "${log_folders[@]}"
 do
     rm -r logs/$log_folder
 done
+rm logs/videos_0/*
 
 default_args="-p --decimals 2 --allowed-distance 0.1 --log-interval 10000 --save-plots"
 
 # Create cat animation
-cargo run --release -- -n 1000000 --shape cat --gaussian $default_args
+cargo run --release -- -n 1000000 --shape cat --gaussian --seed 281 $default_args
 
 # Create dog animation from last cat frame
-cargo run --release -- -p -n 1200000 --shape dog -d logs/cat/output.csv $default_args
+cargo run --release -- -n 1400000 --shape dog -d logs/cat/output.csv $default_args
 
 # # Create whale animation
-cargo run --release -- -p -n 1200000 --shape whale -d logs/dog/output.csv $default_args
+cargo run --release -- -n 4000000 --max-temperature 0.99 --shape whale -d logs/dog/output.csv $default_args
 
 # Create rabiit animation
-cargo run --release -- -p -n 1200000 --shape rabbit -d logs/whale/output.csv $default_args
+cargo run --release -- -n 2000000 --max-temperature 0.6 --shape rabbit -d logs/whale/output.csv $default_args
 
 
 # Create videos
@@ -38,3 +39,6 @@ done
 
 # Concatenate videos
 python3 scripts/concat_videos.py -i ${video_list[@]} -o $video_folder/concat.mp4
+
+# Launch mpv
+mpv $video_folder/concat.mp4
