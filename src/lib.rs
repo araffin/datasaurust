1d478a13eefadfa01e261d1e41fb69a6a517db10
Antonin Raffin <antonin.raffin@ensta.org>
2023-04-10 19:40:53 +0200

    Move optim to separate file and add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
+//! # DatasauRust
+//!
+//! Rust implementation of the algorithm described in the paper:
+//! Same Stats, Different Graphs: Generating Datasets with Varied Appearance and Identical Statistics
+//! through Simulated Annealing.
+//!
+//! Justin Matejka and George Fitzmaurice
+//! ACM CHI 2017
+//!
+//! The paper, video, and associated code and datasets can be found on the Autodesk Research website:
+//! <https://www.autodesk.com/research/publications/same-stats-different-graphs>
+//!
+//! Interactive demo: <https://bqplot.github.io/bqplot-gallery/>
+
 pub mod shapes;
 pub mod types;
+pub mod optim;
