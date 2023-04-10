0238a5a30523c72046092e9bed8aa8a001004e16
Antonin Raffin <antonin.raffin@ensta.org>
2023-04-10 20:34:53 +0200

    [ci skip] Remove unused code

--- scripts/check_csv.py
+++ scripts/check_csv.py
@@ -4,10 +4,3 @@ import pandas as pd
 df = pd.read_csv(sys.argv[1], header=None)
 
 print(df.describe())
-
-# Note: we use biased estimator in the rust code
-# print(df[1].std(ddof=0))
-
-# import ipdb
-
-# ipdb.set_trace()
